c6b3f9ab833ad8a99655c5696b6dd461ed8f2244
dmkaraba <dmkaraba@gmail.com>
2015-07-24 18:12:53 +0300

    add comments

--- multiprocessing/testing.py
+++ multiprocessing/testing.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 def fake_launch(conn, jtasks):
     tasks = json.loads(jtasks)
     for task in tasks:
-        task['done'] = 1
+        task['done'] = 1     # sets 1 for every 'done' key
     conn.send(json.dumps(tasks))
     conn.close()
 
 
-@app.route('/task', methods=['POST'])
+@app.route('/task', methods=['POST'])  # we send [{"id":1, "done":0}, {"id":2,"done":0}]
 def modify_task():
     if request.method == 'POST':
         parent_conn, child_conn = Pipe()
@@ -22,9 +22,12 @@ def modify_task():
         p = Process(target=fake_launch, args=(child_conn, jtasks))
         p.start()
         result = parent_conn.recv()
-        return result
+        return result   # we get [{"id":1, "done":1}, {"id":2,"done":1}]
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# curl -i -H "Content-Type: application/json" -X POST -d "[{"""id""":1, """done""":0},
+#                                                           {"""id""":2,"""done""":0}]' http://127.0.0.1:5000/task
 
